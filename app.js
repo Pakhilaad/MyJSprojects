fe1fb90191db9a2efaca061e353e051385453cfb
pakhi <laadpakhi23@gmail.com>
2021-06-28 23:27:41 +0530

    Color Flipper commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const colors = ["red", "blue", "aqua", "#00FF00", "rgba(255,0,255)"];
+
+// const btn = document.getElementById('btn');
+// const color = document.querySelector('.color');
+
+// btn.addEventListener('click', function() {
+//     const randomNumber = getRandomNUmber();
+//     console.log(randomNumber);
+//     document.body.style.backgroundColor = colors[randomNumber];
+//     color.textContent = colors[randomNumber];
+// });
+
+// function getRandomNUmber() {
+//     return Math.floor(Math.random() * colors.length);
+// }
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
